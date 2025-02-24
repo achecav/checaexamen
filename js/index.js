dbb87d4f1753c2c22a00ae15d7e28b69925afd9a
achecav <achecav@esliceu.net>
2025-02-24 18:44:48 +0100

    recien

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// Exercici 1
+const inputA = document.querySelector('#a');
+const inputB = document.querySelector('#b');
+const spanHipotenusa = document.querySelector('#hipotenusa');
+const btnCalculaHipotenusa = document.querySelector('#calculaHipotenusa');
+const btnNetejaEx1 = document.querySelector('#netejaEx1');
+
+// Exercici 2
+const btnLlanzaDados = document.querySelector('#lanzaDados');
+const btnNetejaEx2 = document.querySelector('#netejaEx2');
+const dau1Img = document.querySelector('#dau1 img');
+const dau2Img = document.querySelector('#dau2 img');
+
+// Exercici 3
+const btnDarkMode = document.querySelector('#darkMode');
+
+// EVENTS
+btnCalculaHipotenusa.onclick = calculaHipotenusa;
+btnNetejaEx1.onclick = netejaEx1;
+btnLlanzaDados.onclick = lanzaDados;
+btnNetejaEx2.onclick = netejaEx2;
+btnDarkMode.onclick = toggleDarkMode;
+
+// FUNCIONS
+function calculaHipotenusa() {
+
+}
+
+function netejaEx1() {
+
+}
+
+let interval = null;
+let num1 = 0;
+let num2 = 0;
+
+function lanzaDados() {
+
+}
+
+function netejaEx2() {
+
+}
+
+function toggleDarkMode() {
+
+}
