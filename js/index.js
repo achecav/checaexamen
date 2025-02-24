d2ad1d231ad7a1ad34db4a37c7736e4bbe085452
achecav <achecav@esliceu.net>
2025-02-24 20:05:57 +0100

    .

--- js/index.js
+++ js/index.js
@@ -23,11 +23,22 @@ btnDarkMode.onclick = toggleDarkMode;
 
 // FUNCIONS
 function calculaHipotenusa() {
+    const a = parseFloat(inputA.value);
+    const b = parseFloat(inputB.value);
 
+    if (isNaN(a) || isNaN(b) || a <= 0 || b <= 0) {
+        alert('Introdueix dos nombres positius.');
+        return;
+    }
+
+    const hipotenusa = Math.sqrt(a ** 2 + b ** 2).toFixed(2);
+    spanHipotenusa.textContent = hipotenusa;
 }
 
 function netejaEx1() {
-
+    inputA.value = 0;
+    inputB.value = 0;
+    spanHipotenusa.textContent = 0;
 }
 
 let interval = null;
@@ -35,13 +46,45 @@ let num1 = 0;
 let num2 = 0;
 
 function lanzaDados() {
-
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+        btnLlanzaDados.textContent = 'Llança';
+        btnLlanzaDados.style.backgroundColor = '';
+    } else {
+        interval = setInterval(() => {
+            const num1 = Math.floor(Math.random() * 6) + 1;
+            const num2 = Math.floor(Math.random() * 6) + 1;
+            dau1Img.src = `/img/dado${num1}.svg`;
+            dau2Img.src = `/img/dado${num2}.svg`;
+        }, 200);
+        btnLlanzaDados.textContent = 'Atura';
+        btnLlanzaDados.style.backgroundColor = 'red';
+    }
+    const num1 = parseInt(dau1Img.src.match(/dado(\d)\.svg/)[1]);
+    const num2 = parseInt(dau2Img.src.match(/dado(\d)\.svg/)[1]);
+    if (num1 === num2) {
+        alert('Enhorabuena');
+    }
 }
 
 function netejaEx2() {
-
+    clearInterval(interval);
+    interval = null;
+    dau1Img.src = './img/dado1.svg';
+    dau2Img.src = './img/dado1.svg';
 }
 
-function toggleDarkMode() {
 
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    const icon = btnDarkMode.querySelector('i');
+}
+    if (document.body.classList.toogle('dark-mode')) {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+    }
+    else {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
 }
